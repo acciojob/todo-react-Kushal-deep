f0b5786257792a1d0adb63ee79443250badb3f2e
Sahebzad Kushal Deep <kushaldeep448@gmail.com>
2025-05-21 14:14:03 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,50 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [inputodo,setInputtodo]=useState("")
+  const [todo,setTodo]=useState([])
+
+
+const handletodo =()=>{
+
+   if (inputodo.trim() !== "") {
+      setTodo([...todo, inputodo]);
+      setInputtodo(""); // clear input after adding
+    }
+
+
+}
+
+
+
+const deletetodo = (indexToDelete) => {
+  const updatedTodos = todo.filter((_, index) => index !== indexToDelete);
+  setTodo(updatedTodos);
+};
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>To-Do-List</h1>
+
+        <input type="text"  value={inputodo} onChange={(e)=>setInputtodo(e.target.value)}></input>
+        <button onClick={handletodo}>Add Todo</button>
+
+        {
+          todo.map((item,index)=>(
+            <div key={index}>
+              <li>{item} </li>
+              <button onClick={() => deletetodo(index)}>Delete</button>
+            </div>
+
+          )
+          )
+
+          
+        }
     </div>
   )
 }
